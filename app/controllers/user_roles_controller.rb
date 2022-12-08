bbd89513eada013ac85366f45f735ccd5c376f65
Steven Chudley <steven.chudley@gmail.com>
2022-12-08 17:36:05 +1300

    Update name of before action

--- app/controllers/user_roles_controller.rb
+++ app/controllers/user_roles_controller.rb
@@ -1,6 +1,6 @@
 class UserRolesController < ApplicationController
   before_action :set_and_authorize_user_role, only: [:show, :update, :destroy]
-  skip_before_action :authorize_base_object!
+  skip_before_action :authorize!
 
   # GET /user_roles
   def index
