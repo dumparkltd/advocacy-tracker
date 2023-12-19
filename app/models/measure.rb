3abc3872e91ab74243c946386f750fa53ba57772
Philip Arndt <git@p.arndt.io>
2023-12-19 20:51:54 +1300

    delete_existing_task_notifications in queue_task_updated_notification if required

--- app/models/measure.rb
+++ app/models/measure.rb
@@ -61,12 +61,18 @@ class Measure < VersionedRecord
     delete_existing_task_notifications!(user_id:)
 
     notifiable_user_measures(user_id:).each do |user_measure|
-      queue_task_updated_notification!(user_id: user_measure.user_id, measure_id: user_measure.measure_id)
+      queue_task_updated_notification!(
+        user_id: user_measure.user_id,
+        measure_id: user_measure.measure_id,
+        delete_existing: false # this is already handled above by delete_existing_task_notifications!
+      )
     end
   end
 
-  def queue_task_updated_notification!(user_id:, measure_id:)
+  def queue_task_updated_notification!(user_id:, measure_id:, delete_existing: true)
     return unless notify?
+
+    delete_existing_task_notifications!(user_id:) if delete_existing
 
     TaskNotificationJob.perform_in(ENV.fetch("TASK_NOTIFICATION_DELAY", 20).to_i.seconds, user_id, measure_id)
   end
@@ -115,5 +121,4 @@ class Measure < VersionedRecord
   def relationship_updated?
     saved_change_to_attribute?(:relationship_updated_at)
   end
-
 end
